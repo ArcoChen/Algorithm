18255ba38425f2cde712db2f97d27a022e802eec
huangjiachang <huangjiachang@wakedata.com>
2023-08-01 00:13:59 +0800

    增加判断平衡二叉树

--- binaryTree/src/main/java/org/example/BalancedBinaryTree.java
+++ binaryTree/src/main/java/org/example/BalancedBinaryTree.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Stack;
+
 /**
  * leetcode 110 平衡二叉树
  *
@@ -28,11 +30,97 @@ public class BalancedBinaryTree {
 
     /**
      * 思考
-     *
-     * 概念：
-     * 二叉树节点的深度：指从根节点到该节点的最长简单路径边的条数。
-     * 二叉树节点的高度：指从该节点到叶子节点的最长简单路径边的条数。
-     *
-     *
+     * <p>
+     * 概念： 二叉树节点的深度：指从根节点到该节点的最长简单路径边的条数。 二叉树节点的高度：指从该节点到叶子节点的最长简单路径边的条数。
      */
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        node3.setLeft(node1);
+        node3.setRight(node2);
+        root.setLeft(node3);
+        root.setRight(node4);
+
+        System.out.println(isBalanced(root));
+    }
+
+    /**
+     * 递归法
+     */
+    public static boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    public static int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+
+        int rightHeight = getHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+
+        // 左右子树高度差大于1，return -1表示已经不是平衡树了
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static boolean isBalanced1(TreeNode node) {
+        if (node == null) {
+            return true;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+
+            TreeNode inNode = stack.peek();
+            // 右结点为null或已经遍历过
+            if (inNode.right == null || inNode.right == pre) {
+                // 输出
+                if (Math.abs(getHeight(inNode.left) - getHeight(inNode.right)) > 1) {
+                    return false;
+                }
+                stack.pop();
+                pre = inNode;
+                node = null;// 当前结点下，没有要遍历的结点了
+            } else {
+                node = inNode.right;// 右结点还没遍历，遍历右结点
+            }
+
+        }
+        return true;
+    }
+
+    /**
+     * 求结点的高度
+     */
+    public int getHeight1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = root.left != null ? root.left.val : 0;
+        int rightHeight = root.right != null ? root.right.val : 0;
+        int height = Math.max(leftHeight, rightHeight) + 1;
+        root.val = height;// 用TreeNode.val来保存当前结点的高度
+        return height;
+    }
 }
---
+++ binaryTree/src/test/java/org/example/LevelAvgOfTheBinaryTreeTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+@author huangjiachang
+@date 2023/7/27
+*/class LevelAvgOfTheBinaryTreeTest {
+
+
+    @Test
+    void averageOfLevels() {
+        TreeNode root = new TreeNode();
+        LevelAvgOfTheBinaryTree.averageOfLevels(root);
+        System.out.println(1);
+    }
+
+    @Test
+    void get(){
+        System.out.println(2);
+    }
+}
